08f891171c843171fb94e02cc8e853a3d8204e26
WS061 <hyt@e-tominaga.com>
2017-06-19 16:08:50 +0900

    Implement algorithm to limit the date

--- icalwage/core.py
+++ icalwage/core.py
@@ -18,35 +18,43 @@ def get_calendar_events(calendar):
             empty_flag = False
             yield component
 
-    if empty_flag is True:
-        raise SystemError("There are not events in this ics file.")
+    # if empty_flag is True:
+    #     raise SystemError("There are not events in this ics file.")
 
 
 def compute_calendar_data(rawdata, args):
 
-    print "args.range_from = '{}'".format(args.range_from)
-    print "args.range_to = '{}'".format(args.range_to)
-
     calendar = Calendar.from_ical(rawdata)
+    csv_data = ""
 
     for event in get_calendar_events(calendar):
 
-        if args.
+        event_start_dt = event.get("dtstart").dt
+        event_end_dt = event.get("dtend").dt
+
+        if args.year:
+
+            if event_start_dt.year == args.year:
+                csv_data += build_csv_data(event_start_dt, event_end_dt)
+
+        elif args.month:
+
+            if event_start_dt.month == args.month:
+                csv_data += build_csv_data(event_start_dt, event_end_dt)
+
+        elif args.range_from and args.range_to:
+
+            if args.range_from < event_start_dt < args.range_to:
+                csv_data += build_csv_data(event_start_dt, event_end_dt)
+
+        else:
+            csv_data += build_csv_data(event_start_dt, event_end_dt)
+
+    return csv_data
 
 
-        print event.get("dtstart").dt
+def build_csv_data(start_dt, end_dt):
 
+    print "date: {}".format(start_dt.strftime(DATE_FORMAT))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return ""
--- icalwage/utils.py
+++ icalwage/utils.py
@@ -3,27 +3,28 @@
 
 import os
 
+from pytz import timezone
 from datetime import datetime
+
+def _get_datetime(query):
+
+    BASIC_ERROR = "Please set full date. ok: '2017.9.1' no: '9.1'"
+
+    fulldate_data = query.split(".")
+
+    if len(fulldate_data) != 3:
+        raise TypeError(BASIC_ERROR)
+
+    year, month, day = fulldate_data
+
+    try:
+        return datetime(int(year), int(month), int(day), tzinfo=timezone('Asia/Tokyo'))
+
+    except:
+        raise TypeError("Incorrect date. " + BASIC_ERROR)
 
 
 def adjust_args(args):
-
-    def _get_datetime(query):
-
-        BASIC_ERROR = "Please set full date. ok: '2017.9.1' no: '9.1'"
-
-        fulldate_data = query.split(".")
-
-        if len(fulldate_data) != 3:
-            raise TypeError(BASIC_ERROR)
-
-        year, month, day = fulldate_data
-
-        try:
-            return datetime(int(year), int(month), int(day))
-
-        except:
-            raise TypeError("Incorrect date. " + BASIC_ERROR)
 
     if args.range_from:
 
@@ -32,6 +33,22 @@ def adjust_args(args):
     if args.range_to:
 
         args.range_to = _get_datetime(args.range_to)
+
+    if args.year:
+
+        try:
+            args.year = int(args.year)
+
+        except:
+            raise TypeError("Please set christian era. ok: '1989' no: '89'")
+
+    if args.month:
+
+        try:
+            args.month = int(args.month)
+
+        except:
+            raise TypeError("Please set integer. ok: '6' no: 'June'")
 
     return args
 

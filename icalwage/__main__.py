6df7e04ff55a41f267b5a646a012c9ca74be3903
WS061 <hyt@e-tominaga.com>
2017-06-19 14:18:36 +0900

    Added the output error process

--- icalwage/__main__.py
+++ icalwage/__main__.py
@@ -7,6 +7,11 @@ from .cli import parser
 from .utils import check_destfile
 
 
+def program(args):
+
+    check_destfile(args.dest, overwrite=args.overwrite)
+
+
 def main(argv=sys.argv):
 
     if len(argv) == 1:
@@ -14,7 +19,15 @@ def main(argv=sys.argv):
         sys.exit(1)
 
     args = parser.parse_args()
-    check_destfile(args.dest, overwrite=args.overwrite)
+
+    try:
+        status = program(args)
+        sys.exit(status)
+
+    except Exception as e:
+        error_type = type(e).__name__
+        sys.stderr.write("{0}: {1}".format(error_type, e.message))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
